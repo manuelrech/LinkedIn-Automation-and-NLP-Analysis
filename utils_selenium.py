0e83bb0e0d158ab58e4620012b9083e641f1f40d
Manuel Rech <63170478+manuelrech@users.noreply.github.com>
2023-01-22 21:03:33 +0100

    settled up utils and new research 1st connection

--- utils_selenium.py
+++ utils_selenium.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from datetime import datetime
 import pandas as  pd
-import utils
+import utils_common
+import utils_api
 import logging
 from time import sleep
 from selenium import  webdriver
@@ -15,23 +16,44 @@ from selenium.webdriver.support import expected_conditions as EC
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_handler = logging.FileHandler('loggers/utils_inmail.log')
+file_handler = logging.FileHandler('loggers/utils_selenium.log')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 logger.info('-'*80)
 
-def linkedin_login(username, password):
+def linkedin_login(profile = None):
+
+    if profile == None:
+        profile = input('what profile do you want to use gialuca/manuel/cappero? ')
+
+    if profile == 'gianluca':
+        username = 'gianluca@vaiuk.finance'
+        password = '2023.VaiLondon$'
+
+    elif profile == 'manuel':
+        username = 'rech.manuel.27@gmail.com'
+        password = 'micael21'
+
+    elif profile == 'cappero':
+        username = 'capperobello111@gmail.com'
+        password = 'micael21'
+
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     ##### LOGIN SESSION #####
     driver.get("https://www.linkedin.com")
+
     username_space = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'session_key')))
     password_space = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'session_password')))
+    
     username_space.send_keys(username)
     password_space.send_keys(password)
+    
     accedi_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'sign-in-form__submit-button')))
     accedi_button.click()
+    
     sleep(10)
     logger.info('successfully logged into linkedin with selenium')
+    
     return driver
 
 def send_in_mail_message():
@@ -76,11 +98,11 @@ def send_in_mail_message():
         object_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[8]/section/div[2]/section/div[2]/form[1]/input')))
         object_field.send_keys('Partnership Proposal by VAI UK Ltd')
 
-        cta, code = utils.randomly_get_message(message, type='cta')
+        cta, code = utils_api.randomly_get_message(message, type='cta')
         core_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.TAG_NAME, 'textarea')))
         core_field.send_keys(cta.format(name))
 
-        row_submitted_inmail = utils.create_row_submitted_inmail(profile_id=profile_id, message_code=code)
+        row_submitted_inmail = utils_api.create_row_submitted_inmail(profile_id=profile_id, message_code=code)
         submitted_inmail.loc[len(submitted_inmail)] = row_submitted_inmail
         submitted_inmail.to_csv('datasets/submitted_inmail.csv', index=0)
         logger.info(f"added row for {profile_id} in submitted in mail")
@@ -89,5 +111,50 @@ def send_in_mail_message():
         # send_button.click()
         logger.info(f'send inmail to {profile_id}')
         driver.switch_to.window(driver.window_handles[0])
+
+def get_new_connections(stopping_id):
+    # stopping id is created just before the campaing starts, creating a cutoff for which we do not need to look more back in time for connections
+    # in simple works, the last connection before the campaign starst. 
+
+    submitted_invitation = pd.read_csv('datasets/submitted_invitation.csv')    
+    network_information = pd.read_csv('datasets/network_info.csv')
+
+    driver = linkedin_login()
+    driver.get("https://www.linkedin.com/mynetwork/invite-connect/connections")
+
+    recently_added_button = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[5]/div[3]/div/div/div/div/div[2]/div/div/main/div/section/div[1]/div[1]/div/button/span')))
+    assert recently_added_button.text == 'Recently added'
+
+    table_connections = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[5]/div[3]/div/div/div/div/div[2]/div/div/main/div/section/div[2]/div[1]')))
+
+    lis = table_connections.find_elements(By.TAG_NAME, 'li')
+
+    for li in lis:
+
+        nome = li.find_element(By.TAG_NAME, 'a')
+        profile_id = nome.get_attribute('href')
+        profile_id = profile_id.split('/')[-2]
+
+        if profile_id == stopping_id:
+            break
+
+        print(profile_id)
+
+        if not submitted_invitation.profile_id.isin([profile_id]).any():
+            continue
+
+        if submitted_invitation.loc[submitted_invitation.profile_id == profile_id, 'accepted_invitation'].iloc[0] == True:
+            continue
+            
+        submitted_invitation.loc[submitted_invitation.profile_id == profile_id, 'accepted_invitation'] = True
+        logger.info(f'{profile_id} has accepted invitation, updated column on submitted invitation')
+        submitted_invitation.to_csv('datasets/submitted_invitation.csv', index=0)
+
+        network_information.loc[network_information.profile_id == profile_id] = utils_common.create_row_network_info(public_identifier=profile_id, connection_level='DISTANCE_1')
+        logger.info(f'network info has been updated to 1 for {profile_id}')
+        network_information.to_csv('datasets/network_info.csv', index=0)
+
+    driver.execute_script("arguments[0].scrollIntoView();", nome)
+
 
 
